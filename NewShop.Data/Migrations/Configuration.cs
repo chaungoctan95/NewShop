791a8b9bf782298c9179495505dabd61e235f816
chaungoctan95 <chaungoctan95@gmail.com>
2017-03-11 16:44:10 +0700

    Fix loi identity va test user Kho qua :(

--- NewShop.Data/Migrations/Configuration.cs
+++ NewShop.Data/Migrations/Configuration.cs
@@ -1,5 +1,8 @@
 namespace NewShop.Data.Migrations
 {
+    using Microsoft.AspNet.Identity;
+    using Microsoft.AspNet.Identity.EntityFramework;
+    using Model.Models;
     using System;
     using System.Data.Entity;
     using System.Data.Entity.Migrations;
@@ -14,18 +17,33 @@ namespace NewShop.Data.Migrations
 
         protected override void Seed(NewShop.Data.NewShopDbContext context)
         {
-            //  This method will be called after migrating to the latest version.
+            var manager = new UserManager<ApplicationUser>(new UserStore<ApplicationUser>(new NewShopDbContext()));
 
-            //  You can use the DbSet<T>.AddOrUpdate() helper extension method 
-            //  to avoid creating duplicate seed data. E.g.
-            //
-            //    context.People.AddOrUpdate(
-            //      p => p.FullName,
-            //      new Person { FullName = "Andrew Peters" },
-            //      new Person { FullName = "Brice Lambson" },
-            //      new Person { FullName = "Rowan Miller" }
-            //    );
-            //
+            var roleManager = new RoleManager<IdentityRole>(new RoleStore<IdentityRole>(new NewShopDbContext()));
+
+            var user = new ApplicationUser()
+            {
+                UserName = "news",
+                Email = "chaungoctan95@gmail.com",
+                EmailConfirmed = true,
+                BirthDay = DateTime.Now,
+                FullName = "13 CNTT"
+
+            };
+
+            manager.Create(user, "123654$");
+
+            if (!roleManager.Roles.Any())
+            {
+                roleManager.Create(new IdentityRole { Name = "Admin" });
+                roleManager.Create(new IdentityRole { Name = "User" });
+            }
+
+            var adminUser = manager.FindByEmail("chaungoctan95@gmail.com");
+
+            manager.AddToRoles(adminUser.Id, new string[] { "Admin", "User" });
+
         }
     }
 }
+
