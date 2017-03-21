b24b8776e2d56032af7aacc73cce817fedc5cf28
chaungoctan95 <chaungoctan95@gmail.com>
2017-03-21 16:34:04 +0700

    Tiềm kiếm autocomplete với thư viện jquery-ui

--- NewShop.Web/App_Start/RouteConfig.cs
+++ NewShop.Web/App_Start/RouteConfig.cs
@@ -7,43 +7,50 @@ using System.Web.Routing;
 
 namespace NewShop.Web
 {
-    public class RouteConfig
-    {
-        public static void RegisterRoutes(RouteCollection routes)
+     public class RouteConfig
         {
-            routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
-            routes.MapRoute(
-         name: "Login",
-         url: "dang-nhap.html",
-         defaults: new { controller = "Account", action = "Login", id = UrlParameter.Optional },
-         namespaces: new string[] { "NewShop.Web.Controllers" }
-     );
-            routes.MapRoute(
-           name: "About",
-           url: "gioi-thieu.html",
-           defaults: new { controller = "About", action = "Index", id = UrlParameter.Optional },
-           namespaces: new string[] { "NewShop.Web.Controllers" }
-       );
-            routes.MapRoute(
-             name: "Product Category",
-             url: "{alias}.pc-{id}.html",
-             defaults: new { controller = "Product", action = "Category", id = UrlParameter.Optional },
-               namespaces: new string[] { "NewShop.Web.Controllers" }
-         );
+            public static void RegisterRoutes(RouteCollection routes)
+            {
+                routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 
-            routes.MapRoute(
-             name: "Product",
-             url: "{alias}.p-{productId}.html",
-             defaults: new { controller = "Product", action = "Detail", productId = UrlParameter.Optional },
-               namespaces: new string[] { "NewShop.Web.Controllers" }
+                routes.MapRoute(
+          name: "Search",
+          url: "tim-kiem.html",
+          defaults: new { controller = "Product", action = "Search", id = UrlParameter.Optional },
+          namespaces: new string[] { "NewShop.Web.Controllers" }
+      );
+                routes.MapRoute(
+             name: "Login",
+             url: "dang-nhap.html",
+             defaults: new { controller = "Account", action = "Login", id = UrlParameter.Optional },
+             namespaces: new string[] { "NewShop.Web.Controllers" }
          );
+                routes.MapRoute(
+               name: "About",
+               url: "gioi-thieu.html",
+               defaults: new { controller = "About", action = "Index", id = UrlParameter.Optional },
+               namespaces: new string[] { "NewShop.Web.Controllers" }
+           );
+                routes.MapRoute(
+                 name: "Product Category",
+                 url: "{alias}.pc-{id}.html",
+                 defaults: new { controller = "Product", action = "Category", id = UrlParameter.Optional },
+                   namespaces: new string[] { "NewShop.Web.Controllers" }
+             );
 
-            routes.MapRoute(
-                name: "Default",
-                url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional },
-                  namespaces: new string[] { "NewShop.Web.Controllers" }
-            );
+                routes.MapRoute(
+                 name: "Product",
+                 url: "{alias}.p-{productId}.html",
+                 defaults: new { controller = "Product", action = "Detail", productId = UrlParameter.Optional },
+                   namespaces: new string[] { "NewShop.Web.Controllers" }
+             );
+
+                routes.MapRoute(
+                    name: "Default",
+                    url: "{controller}/{action}/{id}",
+                    defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional },
+                      namespaces: new string[] { "NewShop.Web.Controllers" }
+                );
+            }
         }
     }
-}
